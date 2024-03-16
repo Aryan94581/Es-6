526807b3d21fc0a3d0795d675082e78423b87b11
AryanSwami <swamiaryan611@gmail.com>
2024-03-16 18:50:58 +0530

    a

--- includes.js
+++ includes.js
@@ -1,4 +1,4 @@
 let numarray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numarray.includes(8));
-//by using includes function we can check wether it is i the erray or not
+//by using includes function we can check wether it is in the erray or not
 // it is the shortest way to find the value in the erray or
